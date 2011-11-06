ba90007fc11cf41eba5b76de333c9705d294163e
akin <akin@localhost>
2011-11-07 00:58:22 +0200

    main function

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : winice.cpp
+// Author      : Mikael Korpela
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "ice.h"
+#include <iostream>
+#include <system/os> // if you need different entry point for some OS, than main, use ifdefs from here.
+
+int main( int argc , char *argv[] )
+{
+	std::cout << "WinIce starting" << std::endl;
+	return iceMain( argc , argv );
+}
